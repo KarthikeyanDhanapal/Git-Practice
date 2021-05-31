ef7e097d962925a3af67682ee8422e165bdd352a
Karthikeyan <karthikeyan.dpr@gmail.com>
2021-05-31 19:53:39 +0530

    added FileWordCount.py

---
+++ FileWordCount.py
@@ -0,0 +1,9 @@
+import sys
+
+def word_count(inputfile):
+   with open(inputfile,'r') as file:
+     data = file.read()
+     data.replace(","," ")
+   return (len(data.split(" "))+1)
+
+print("Total word count of the file "+str(sys.argv[1])+ " is " + str(word_count(sys.argv[1])))
