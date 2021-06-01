13bac489bbbba6a5ff504e366f35ef4bb0a5b302
Karthikeyan <karthikeyan.dpr@gmail.com>
2021-06-01 22:12:02 +0530

    Added line comments in FileWordCount.py

--- FileWordCount.py
+++ FileWordCount.py
@@ -1,10 +1,11 @@
 #This program takes input as text file and prints the total word count
+#it does not count the very first word in each new line - need to use REGEX
 import sys
 
 def word_count(inputfile):
-   with open(inputfile,'r') as file:
+   with open(inputfile,'r') as file:  #with handles exceptions automatically
      data = file.read()
-     data.replace(","," ")
-   return (len(data.split(" "))+1)
+     data.replace(","," ")            #Just "" will have no delimiter, have it as " "
+   return (len(data.split(" ")))    #As the count is based on blank space, the very first word is to be accounted
 
 print("Total word count of the file "+str(sys.argv[1])+ " is " + str(word_count(sys.argv[1])))
