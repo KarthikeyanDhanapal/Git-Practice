dfb086a20f8ae1af49a015ea67906c36584938be
Karthikeyan <karthikeyan.dpr@gmail.com>
2021-06-02 15:09:50 +0530

    fix logic for word begin in newline

--- FileWordCount.py
+++ FileWordCount.py
@@ -6,6 +6,8 @@ def word_count(inputfile):
    with open(inputfile,'r') as file:  #with handles exceptions automatically
      data = file.read()
      data.replace(","," ")            #Just "" will have no delimiter, have it as " "
-   return (len(data.split(" ")))    #As the count is based on blank space, the very first word is to be accounted
+     blank=len(data.split(" "))
+     newline=len(data.split("\n"))
+   return(int(blank+newline))    #As the count is based on blank space, the very first word is to be accounted
 
 print("Total word count of the file "+str(sys.argv[1])+ " is " + str(word_count(sys.argv[1])))
